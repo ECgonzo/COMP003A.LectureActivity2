17335e648187652ea5350475d12abaa947d5b2d8
ethengonzalez <ethengonzalez@LEM278-05>
2024-01-24 18:41:56 -0800

    Add project files.

---
+++ COMP003A.LectureActivity2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Author: Ethen Gonzalez
+ * Course: COMP-003A
+ * Purpose: Lecture activity for variables, type system, Math, and Console properties
+ * Reference: The C# Player's Guide (4e) by RB Whitaker
+ */
+
+namespace COMP003A.LectureActivity2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
